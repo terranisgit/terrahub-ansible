58f0aec6cb4b8798631efc1ce6376898e8d49dfd
Cyril Gallay <c.gallay@gmail.com>
2016-02-25 15:45:18 +0100

    Using a specific certificat for apache

--- uninstall.sh
+++ uninstall.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-ansible-playbook -t cleanup -e "cleanup=true" playbooks/terrahub.yml
+ansible-playbook $@ -t cleanup -e "cleanup=true" playbooks/terrahub.yml 
+
+sudo apt-get purge -y ldap-utils slapd
